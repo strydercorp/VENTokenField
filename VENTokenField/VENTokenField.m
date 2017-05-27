2658a0179f548f978c5a2f92984fe3a3245155e2
David Liu <david.shuang.liu@gmail.com>
2017-05-26 19:01:34 -0700

    revert the uncommit

--- VENTokenField/VENTokenField.m
+++ VENTokenField/VENTokenField.m
@@ -551,9 +551,7 @@ static const CGFloat VENTokenFieldDefaultMaxHeight          = 150.0;
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
     if ([self.delegate respondsToSelector:@selector(tokenField:didEnterText:)]) {
-        if ([textField.text length]) {
             [self.delegate tokenField:self didEnterText:textField.text];
-        }
     }
     
     return NO;
